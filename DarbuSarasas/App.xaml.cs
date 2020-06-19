8d7e576fd21632d5cb669bcd6c6694bcc19d437e
SergejMaz <64395909+SergejMaz@users.noreply.github.com>
2020-06-19 12:19:12 +0300

    Add project files.

---
+++ DarbuSarasas/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Markup;
+using System.Windows.Navigation;
+
+namespace DarbuSarasas
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnLoadCompleted(NavigationEventArgs e)
+        {
+            var cultureInfo = new CultureInfo("lt-LT");
+            Thread.CurrentThread.CurrentCulture = cultureInfo;
+            Thread.CurrentThread.CurrentUICulture = cultureInfo;
+            CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
+            CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
+            FrameworkElement.LanguageProperty.OverrideMetadata(typeof(FrameworkElement),
+                new FrameworkPropertyMetadata(XmlLanguage.GetLanguage(CultureInfo.CurrentCulture.IetfLanguageTag)));
+
+            base.OnLoadCompleted(e);    
+        }
+    }
+}
---
+++ DarbuSarasas/Models/TodoModel.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DarbuSarasas.Models
+{
+    class TodoModel : INotifyPropertyChanged
+    {
+        public DateTime CreatioDate { get; set; } = DateTime.Now;
+
+        private bool _isDone;
+        private string _text;
+
+        [JsonProperty(PropertyName = "creationData1")]
+
+        public bool IsDone
+        {
+            get { return _isDone; }
+            set 
+            {
+                if (_isDone == value)
+                    return;
+                
+                _isDone = value;
+                OnPropertyChanged("IsDone");
+            }
+        }
+        [JsonProperty(PropertyName = "creationData")]
+        public string Text
+        {
+            get { return _text; }
+            set 
+            {
+                if (_text == value)
+                    return;
+                _text = value;
+                OnPropertyChanged("Text");
+
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName = "")
+        {            
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            
+
+        }
+
+
+    }
+}
